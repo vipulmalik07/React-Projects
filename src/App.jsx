e4116d66d6037da7f40b444a0cc909032d9a289e
vipulmalik07 <chvipulmalik07@gmail.com>
2024-08-05 12:52:49 +0530

    Project added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Weather from './Components/Weather'
+
+const App = () => {
+  return (
+    <div className='app'>
+        <Weather/>
+    </div>
+  )
+}
+
+export default App
